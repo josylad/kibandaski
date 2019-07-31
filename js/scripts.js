63eec812e52a6c4b2e4381b9288b4bb2306bbff5
Joseph Adediji <josylado@gmail.com>
2019-07-31 16:25:37 +0300

    add JS for printing phone number into the order summary

--- js/scripts.js
+++ js/scripts.js
@@ -105,7 +105,7 @@ $("#order").submit(function() {
 //   // $("#address1").text(address)
 //
   document.getElementById("address1").innerHTML = ("Address: ") + address;
-//   document.getElementById("phone1").innerHTML = ("Phone: ") + phone;
+  document.getElementById("phone1").innerHTML = ("Phone: ") + phone;
 //   document.getElementById("output").innerHTML = ("Your order Total is = ") + result + (" KES");
 //   alert("Your total order is " + result + " KES." + " Thank You for Ordering from KIBANDASKI! Your Order is on it's way!");
 //   $("form#checkout").css('data-dismiss','modal');
